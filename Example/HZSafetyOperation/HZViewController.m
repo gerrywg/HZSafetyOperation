264a808f326bdfdb47fc475b037fe190f231a16c
gerrywg <chwanggang@msn.com>
2018-10-24 17:09:36 +0800

    first commit

--- Example/HZSafetyOperation/HZViewController.m
+++ Example/HZSafetyOperation/HZViewController.m
@@ -18,6 +18,39 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    NSMutableArray *arr1 = [NSMutableArray array];
+    
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        for (int i = 0; i<10000; i++) {
+            //[array addObject:@(i)];
+            //NSLog(@"%@",[array fetchArray].lastObject);
+            
+//            [arr1 safeUpdateBlock:^{
+//                [arr1 addObject:@(i)];
+//            }];
+//
+//            [arr1 safeReadBlock:^{
+//                NSLog(@"%@",arr1.lastObject);
+//            }];
+        }
+        
+    });
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        for (int i = 0; i<10000; i++) {
+            //[array addObject:[NSString stringWithFormat:@"A--%@",@(i)]];
+            //NSLog(@"%@",[array fetchArray].lastObject);
+//            [arr1 safeUpdateBlock:^{
+//                [arr1 addObject:[NSString stringWithFormat:@"A--%@",@(i)]];
+//            }];
+//
+//            [arr1 safeReadBlock:^{
+//                NSLog(@"--%@",arr1.lastObject);
+//            }];
+            
+        }
+        
+    });
 }
 
 - (void)didReceiveMemoryWarning
